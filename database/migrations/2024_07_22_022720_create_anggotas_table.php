08c2e5ff5a9f10ecdd0226ecb7a552e441098395
adisalaras <adisalaras41@gmail.com>
2024-07-22 14:28:47 +0700

    fix: Add field profile on anggota

--- database/migrations/2024_07_22_022720_create_anggotas_table.php
+++ database/migrations/2024_07_22_022720_create_anggotas_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('alamat');
             $table->string('kota');
             $table->string('email');
+            $table->string('profile');
             $table->softDeletes();
             $table->timestamps();
         });
