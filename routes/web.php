948f22cfefe9ebebfd61e771ae0a267dff863daf
adisalaras <adisalaras41@gmail.com>
2024-07-22 13:19:06 +0700

    feat: Add route on dashboard (anggota and buku)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AnggotaController;
+use App\Http\Controllers\BukuController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PeminjamanController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+
+Route::resource('anggota', AnggotaController::class); 
+Route::resource('buku', BukuController::class );
+Route::resource('peminjaman', PeminjamanController::class);
+
